30cb54154d9f25b5b8785b18e7442cce8f212f91
Mike Kelly <mikechriskelly@gmail.com>
2014-05-02 00:05:00 -0700

    Simple requirejs integration amended

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,7 @@ require.config({
     'jQuery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
     'angular': '//code.angularjs.org/1.2.0-rc.2/angular',
     'Dropbox': '//cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.2/dropbox.min',
-    'requirejs': '//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.8/require'
+    'ItemMirror': 'http://keepingfoundthingsfound.com/apps/_shared/ItemMirror.min'
   },
   shim: {
     'angular' : {'exports' : 'angular'},
@@ -14,6 +14,7 @@ require.config({
 
 require(['jQuery', 'angular', 'Dropbox', 'ItemMirror', 'app'] , function ($, angular, Dropbox, ItemMirror, app) {
   'use strict';
+
   $(function () { // using jQuery because it will run this even if DOM load already happened
     angular.bootstrap(document, ['app']);
   });
