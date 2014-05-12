d050dcd8bc72095a23ec28ec61a380639e504614
Mike Kelly <mikechriskelly@gmail.com>
2014-05-12 13:24:06 -0700

    Added Hallo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,28 @@
 require.config({
   baseUrl: '/scripts',
   paths: {
-    'jQuery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
+    'jQuery': '../bower_components/jquery/jquery.min',
+    'jQuery-UI': '../bower_components/jquery-ui/ui/jquery-ui',
     'angular': '../bower_components/angular/angular',
     'angular-route': '../bower_components/angular-route/angular-route',
     'Dropbox': '//cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.2/dropbox.min',
-    'ItemMirror': 'http://keepingfoundthingsfound.com/apps/_shared/ItemMirror.min'
+    'ItemMirror': 'http://keepingfoundthingsfound.com/apps/_shared/ItemMirror.min',
+    'Hallo': '../bower_components/hallo.js/hallo',
+    'Rangy': '../bower_components/rangy/rangy-core'
   },
   shim: {
     'angular' : {'exports' : 'angular'},
     'angular-route': ['angular'],
     'jQuery': {'exports' : 'jQuery'},
     'Dropbox' : {'exports': 'Dropbox'},
+    'Hallo': ['jQuery', 'jQuery-UI' ,'Rangy']
     //'ItemMirror' : {'exports': 'ItemMirror'}
   },
   priority: ['angular']
 });
 
 // Initialize the app
-require(['jQuery', 'angular', 'angular-route', 'Dropbox', 'ItemMirror', 'app', 'routes'] , function ($, angular, Dropbox, ItemMirror, app, routes) {
+require(['jQuery', 'angular', 'angular-route', 'Dropbox', 'ItemMirror', 'Hallo', 'app', 'routes'] , function ($, angular, Dropbox, ItemMirror, Hallo, app, routes) {
   'use strict';
 
   // Confirm dependencies
