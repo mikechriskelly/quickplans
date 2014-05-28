a3a9ba7da0fbae7bcd46cdb4afaaac00d5e2872b
Mike Kelly <mikechriskelly@gmail.com>
2014-05-28 01:53:15 -0700

    Added UI tree

--- app/scripts/controllers/main-controller.js
+++ app/scripts/controllers/main-controller.js
@@ -22,6 +22,75 @@ define(['./module','angular'],
                 //Bind results to scope
                 console.log(result);
                $scope.associations = result;
+
+               $scope.list = [];
+          
+               for(var i=0; i < result.length; i++) {
+                  var item={};
+                  item.id = i;
+                  item.title = result[i];
+                  item.items = [];
+                  $scope.list.push(item);
+               }
+
+               console.log($scope.list);
+
+              // $scope.list = [{
+              //   "id": 1,
+              //   "title": "1. dragon-breath",
+              //   "items": []
+              // }, {
+              //   "id": 2,
+              //   "title": "2. moiré-vision",
+              //   "items": [{
+              //     "id": 21,
+              //     "title": "2.1. tofu-animation",
+              //     "items": [{
+              //       "id": 211,
+              //       "title": "2.1.1. spooky-giraffe",
+              //       "items": []
+              //     }, {
+              //       "id": 212,
+              //       "title": "2.1.2. bubble-burst",
+              //       "items": []
+              //     }],
+              //   }, {
+              //     "id": 22,
+              //     "title": "2.2. barehand-atomsplitting",
+              //     "items": []
+              //   }],
+              // }, {
+              //   "id": 3,
+              //   "title": "3. unicorn-zapper",
+              //   "items": []
+              // }, {
+              //   "id": 4,
+              //   "title": "4. romantic-transclusion",
+              //   "items": []
+              // }];
+
+              $scope.selectedItem = {};
+
+              $scope.options = {
+              };
+
+              $scope.remove = function(scope) {
+                scope.remove();
+              };
+
+              $scope.toggle = function(scope) {
+                scope.toggle();
+              };
+
+              $scope.newSubItem = function(scope) {
+                var nodeData = scope.$modelValue;
+                nodeData.items.push({
+                  id: nodeData.id * 10 + nodeData.items.length,
+                  title: nodeData.title + '.' + (nodeData.items.length + 1),
+                  items: []
+                });
+              };
+
                $scope.status = 'success';
                $scope.loaded = true;
                $scope.GUIDs = im.GUIDs;
